2178f7487e7058af4dfbcf1aeb9d97a8db73eff2
“daffadev-inc” <daffadev.inc@gmail.com>
2022-06-16 00:18:31 +0700

    first

---
+++ _posts/2022-3-7-Hello-WorldKopi.md
@@ -0,0 +1,12 @@
+---
+layout: post
+title: Helo, You're up and running!
+image: https://ik.imagekit.io/x1666/default-image.jpg?updatedAt=1655310737203&tr=w-640,h-360,fo-custom,cm-extract
+excerpt_separator: <!--more-->
+---
+
+Next you can update your site name, avatar and other options using the _config.yml<!--more--> file in the root of your repository (shown below).
+
+![_config.yml]({{ site.baseurl }}/images/config.png)
+
+The easiest way to make your first post is to edit this one. Go into /_posts/ and update the Hello World markdown file. For more instructions head over to the [Jekyll Now repository](https://github.com/barryclark/jekyll-now) on GitHub.
---
+++ _site/sw.js
@@ -0,0 +1,55 @@
+importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn/workbox/workbox-sw.js');
+
+const { registerRoute } = workbox.routing;
+const { CacheFirst, NetworkFirst, StaleWhileRevalidate } = workbox.strategies;
+const { CacheableResponse } = workbox.cacheableResponse;
+
+workbox.core.setCacheNameDetails({
+  prefix: 'http://localhost:4000',
+  suffix: '2022-06'
+});
+
+registerRoute(
+  '/',
+  new NetworkFirst()
+);
+
+registerRoute(
+  /page[0-99]/,
+  new NetworkFirst()
+)
+
+registerRoute(
+  new RegExp('/\\d{4}/\\d{2}/\\d{2}/.+'),
+  new StaleWhileRevalidate()
+)
+
+workbox.precaching.precacheAndRoute([
+  { url: '/Hello-WorldKopi/', revision: '2022-03-07' },
+  { url: '/Hello-Worldcopy/', revision: '2014-03-07' },
+  { url: '/Hello-copy1/', revision: '2014-03-06' },
+  { url: '/Hello-World1/', revision: '2014-03-05' },
+  { url: '/Hello-copy/', revision: '2014-03-04' },
+  { url: '/Hello-World/', revision: '2014-03-03' },
+  { url: '/Hello/', revision: '2014-03-02' },
+  { url: '/Worldcopy/', revision: '2014-03-01' },
+  { url: '/', revision: '202206152335' },
+  { url: '/shop/', revision: '202206152335' },
+  { url: '/about/', revision: '202206152335' },
+  { url: '/assets/css/style.css', revision: '202206152335' },
+  { url: '/assets/css/spinner.css', revision: '202206152335' }
+])
+
+registerRoute(
+  ({request}) => request.destination === 'image' ,
+  new CacheFirst({
+    plugins: [
+      new CacheableResponse({statuses: [0, 200]})
+    ],
+  })
+);
+
+registerRoute(
+  /\/(images|favicon|css)/,
+  new CacheFirst()
+);
